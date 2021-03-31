8c86955b7b260ff1c59d7e92f2ca129d63e5d543
CanDagdelen94 <candagdelenn@gmail.com>
2021-03-31 18:38:35 +0300

    Reinforcement Learning Example

---
+++ Environment.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+from PolicyGradients import PolicyGradients
+
+if __name__ == '__main__':
+    data1 = pd.read_csv(r"...\daily_adjusted_AAPL.csv")
+    #print(data)
+    df1 = data1["adjusted_close"][::-1]
+    df1.index = df1.index[::-1]
+    df1 = df1.to_numpy()
+    data2 = pd.read_csv(r"...\daily_IBM.csv")
+    #print(data)
+    df2 = data2["adjusted_close"][::-1]
+    df2.index = df2.index[::-1]
+    df2 = df2.to_numpy()
+
+    def rewardfunction(self, action, resultbefore, resultnow):
+            if action == 0:
+                diff = resultnow - resultbefore
+                if diff >0:
+                    reward = 0
+                else:
+                    reward = diff
+            else:
+                diff = resultnow - resultbefore
+                if diff <0:
+                    reward = 0
+                else:
+                    reward = -diff
+            return reward
+
+    model = PolicyGradients()
+    
+    x = df1
+    episodes = 50
+    batch_train_mode = True
+    zeroc=0
+    days=60
+    for e in range(0,episodes+1):
+        model.restart_transition()
+        totalreward = 0
+        for i in range(days, len(x)):
+            state = x[i-days:i]
+            eps = 1
+            state = (state - np.mean(state)) / (np.std(state) + eps)
+            action = model.choose_action(state)
+            resultbefore = x[i-1]
+            resultnow = x[i]
+            reward = model.rewardfunction(action, resultbefore, resultnow)
+            model.store_transition(state, action, reward)
+            totalreward += reward
+        model.learn(batch_train_mode)
+        
+        if totalreward==0:
+            if zeroc == 20:
+                break
+            zeroc+=1
+        if totalreward != 0:
+            zeroc = 0
+        print("Totalreward :", totalreward, "episode :", e)
---
+++ PolicyGradients.py
@@ -0,0 +1,77 @@
+from tensorflow import keras
+from tensorflow.keras import backend as k
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+class PolicyGradients:
+    def __init__(self, inputdims, actiondims, layers, neurons):
+        inputdims = inputdims
+        self.actiondims = actiondims
+        layers = layers
+        neurons = neurons
+        self.model = self.buildnetwork(inputdims, self.actiondims, layers, neurons)
+        
+    def buildnetwork(self, inputdims, outputdims, layers, neurons):
+        model = keras.Sequential()
+        model.add(keras.Input(shape=(inputdims,)))
+        for l in range(layers):
+            model.add(keras.layers.Dense(neurons, activation = keras.activations.relu))
+        model.add(keras.layers.Dense(outputdims, activation = keras.activations.softmax))
+        
+        optimizer = keras.optimizers.Adamax()
+        model.compile(optimizer = optimizer)
+        return model
+
+    def choose_action(self, state):
+        state = tf.expand_dims(state,0)
+        probs = self.model(state)
+        action = np.argmax(probs, axis = 1)[0]
+        return action
+    
+    def store_transition(self, state, action, reward):
+        self.state_memory.append(state)
+        self.action_memory.append(action)
+        self.reward_memory.append(reward)
+        
+    def restart_transition(self):
+        self.state_memory = []
+        self.action_memory = []
+        self.reward_memory = []
+        
+    def customloss(self, action, prob, delta, batch_train):
+        out = k.clip(prob, 1e-8, 1-1e-8)
+        if batch_train:
+            loglik = action * -k.log(out)
+            loglik = k.sum(loglik, axis=1) * delta
+            loglik = k.sum(loglik) / len(loglik)
+            #print(loglik)
+        else:
+            loglik = action * -k.log(out)
+            loglik = k.sum(loglik, axis=1) * delta
+        return loglik
+    
+    def learn(self, batch_train = True):
+        state_memory = np.array(self.state_memory)
+        action_memory = np.array(self.action_memory)
+        reward_memory = np.array(self.reward_memory)
+        
+        actions = np.zeros( [len(action_memory),self.actiondims] )
+        actions[ np.arange(len(action_memory)), action_memory ] = 1
+        
+        if batch_train:
+            with tf.GradientTape() as tape:
+                probs = self.model(state_memory)
+                loss = self.customloss(actions, probs, reward_memory, batch_train)
+            grad = tape.gradient(loss, self.model.trainable_variables)
+            self.model.optimizer.apply_gradients(zip(grad, self.model.trainable_variables))
+        else:
+            for i in range(len(state_memory)):
+                with tf.GradientTape() as tape:
+                    pred = self.model(state_memory[i:i+1])
+                    loss = self.customloss(actions[i], pred, reward_memory[i], batch_train)
+                grad = tape.gradient(loss, self.model.trainable_variables)
+                self.model.optimizer.apply_gradients(zip(grad, self.model.trainable_variables))
+
+if __name__  == "__main__":
+    model = PolicyGradients()
